5ebc694b423ae32f9a4e13b553b57a826eb87a98
DESKTOP-20R18M5\Ariel Calli <arielcallisaya00@gmail.com>
2018-12-06 01:21:23 -0300

    add google ads

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,13 @@
         }
 
     </style>
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({
+          google_ad_client: "ca-pub-6770941823425032",
+          enable_page_level_ads: true
+     });
+</script>
 </head>
 <body>
 
