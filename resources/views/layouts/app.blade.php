966aa0f706fb8dcca49ee6f42e9779d3bd8a1fa2
DESKTOP-20R18M5\Ariel Calli <arielcallisaya00@gmail.com>
2018-12-10 17:23:28 -0300

    ads

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,11 +26,12 @@
 
     </style>
     <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <script>
-     (adsbygoogle = window.adsbygoogle || []).push({
-          google_ad_client: "ca-pub-6770941823425032",
-          enable_page_level_ads: true
-     });
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-6770941823425032",
+    enable_page_level_ads: true
+  });
 </script>
 <script async custom-element="amp-auto-ads"
         src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js">
